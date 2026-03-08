e7915d1427c95feca44eec182301da4d4d893bcd
Bruno Unterberger <bruno.unterberger@gmail.com>
2026-03-08 10:46:44 +0100

    Suppression des éléments en trop et ajout du gitignore

--- src/Zorg320.Utilitaire.FichierTemporaire.Noyau/obj/Debug/net8.0/Zorg320.Utilitaire.FichierTemporaire.Noyau.AssemblyInfo.cs
+++ src/Zorg320.Utilitaire.FichierTemporaire.Noyau/obj/Debug/net8.0/Zorg320.Utilitaire.FichierTemporaire.Noyau.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zorg320.Utilitaire.FichierTemporaire.Noyau")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c930b6de59cb2f2a9536610c3cbd3280c6ddb85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+313c7c8b528b721b228e0a83e378468985309acd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zorg320.Utilitaire.FichierTemporaire.Noyau")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zorg320.Utilitaire.FichierTemporaire.Noyau")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
